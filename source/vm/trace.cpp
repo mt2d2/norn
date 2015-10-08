a8d60557ed4748455f7d57224b165a0cd97b9dc6
Michael Tremel <itrekkie@gmail.com>
2015-10-08 17:51:30 -0500

    add trace files! duh

---
+++ source/vm/trace.cpp
@@ -0,0 +1,81 @@
+#include "trace.h"
+
+#if !NOJIT
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "common.h"
+#include "block.h"
+#include "program.h"
+#include "instruction.h"
+
+Trace::Trace()
+    : last_state(Trace::State::ABORT),
+      instructions(std::vector<const Instruction *>()),
+      traceExits(std::vector<uint64_t>()),
+      calls(std::map<const Block *, unsigned int>()), nativePtr(nullptr) {}
+
+Trace::~Trace() {
+  if (nativePtr != nullptr)
+    raise_error("nativePtr should remain nullptr");
+}
+
+Trace::State Trace::record(const Instruction *i) {
+  if (is_head(i)) {
+    last_state = Trace::State::COMPLETE;
+    goto exit;
+  }
+
+  if (i->op == LOOP && instructions.size() > 0 &&
+      i->arg.l != instructions[0]->arg.l) {
+    // encountered another loop, abort
+    last_state = Trace::State::ABORT;
+    goto exit;
+  }
+
+  instructions.push_back(i);
+  last_state = Trace::State::TRACING;
+
+exit:
+  return last_state;
+}
+
+bool Trace::is_complete() const { return last_state == Trace::State::COMPLETE; }
+
+bool Trace::is_head(const Instruction *i) const {
+  return instructions.size() > 0 && i == instructions[0];
+}
+
+void Trace::debug() const {
+  for (auto *i : instructions) {
+    std::cout << *i << std::endl;
+  }
+}
+
+void Trace::compile(const bool debug) {}
+
+nativeTraceType Trace::get_native_ptr() const { return nativePtr; }
+
+uint64_t Trace::get_trace_exit(int offset) const { return traceExits[offset]; }
+
+std::map<const Block *, unsigned int> Trace::get_trace_calls() const {
+  return calls;
+}
+
+void Trace::identify_trace_exits() {
+  unsigned int bytecodePosition = 0;
+
+  for (const auto *i : instructions) {
+    if (i->op == FJMP || i->op == TJMP) {
+      traceExits.push_back(bytecodePosition);
+    }
+
+    ++bytecodePosition;
+  }
+}
+
+#endif // !NOJIT
---
+++ source/vm/trace.h
@@ -0,0 +1,55 @@
+#ifndef TRACE_H
+#define TRACE_H
+
+#if !NOJIT
+
+#include <vector>
+#include <map>
+
+typedef void (*nativeTraceType)(/*trace exit*/ int64_t *,
+                                /*stack adjust*/ int64_t *,
+                                /*stack*/ int64_t *,
+                                /*memory*/ int64_t *);
+
+struct Instruction;
+class Block;
+
+class Trace {
+public:
+  struct LangLocal {
+    unsigned int memPosition;
+    unsigned int memOffsetPosition;
+  };
+
+  enum State {
+    ABORT,
+    TRACING,
+    COMPLETE,
+  };
+
+  Trace();
+  ~Trace();
+  State record(const Instruction *i);
+  bool is_complete() const;
+  void debug() const;
+  void compile(const bool debug);
+  nativeTraceType get_native_ptr() const;
+  uint64_t get_trace_exit(int offset) const;
+  std::map<const Block *, unsigned int> get_trace_calls() const;
+
+private:
+  bool is_head(const Instruction *i) const;
+  void jit(const bool debug);
+  void identify_trace_exits();
+  void identify_trace_calls();
+
+  State last_state;
+  std::vector<const Instruction *> instructions;
+  std::vector<uint64_t> traceExits;
+  std::map<const Block *, unsigned int> calls;
+  nativeTraceType nativePtr;
+};
+
+#endif // !NOJIT
+
+#endif // TRACE_H
