c64e3cda22b05e0cb93ff3a5a5c0b70b7b4c5d66
Michael Tremel <itrekkie@gmail.com>
2015-11-02 22:54:57 -0700

    comparison

---
+++ test/basic/jit.lua
@@ -0,0 +1,22 @@
+local function maths()
+  return (-10 + (2 + 2) * 20)
+end
+
+local function jit_test(m, t)
+  local ret = 0
+  for i=1,10000000 do
+    ret = ret + m + t + maths()
+  end
+  return ret
+end
+
+local function float_maths()
+  return 4.0 + 4.0
+end
+
+local function jit_float_test()
+  return 8.4 + float_maths()
+end
+
+print(jit_test(4, 4))
+print(jit_float_test())
